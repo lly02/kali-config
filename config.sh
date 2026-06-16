48b898ec363e0d942ff8c0a22477348d75161221
yen <lly.02@outlook.com>
2026-06-16 13:47:45 +0800

    Update .zshrc to source .p10k.zsh conditionally
    Added sourcing of .p10k.zsh if it exists to .zshrc.

--- config.sh
+++ config.sh
@@ -1775,5 +1775,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 perl -0777 -pi -e 's/^plugins=\(.*?\)/plugins=(\n    zsh-autosuggestions\n    zsh-syntax-highlighting\n)/ms' ~/.zshrc
+echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc
 
 echo "Configuration done. Restart shell."
